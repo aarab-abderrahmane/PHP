e592c21934a0ac1b5a3fc8f5acccd68cddaf7fbf
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-04-04 21:01:00 +0000

    analysis-numbers-entered #1

---
+++ Exercices/Forms/analysis-numbers-entered.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    
+</head>
+<body style=
+        "background-color:#0e1827;
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:center;
+        height:100vh;
+        ">
+    
+
+
+    <?php
+
+        if($_SERVER['REQUEST_METHOD']== "POST" && isset($_POST['text'])){
+
+            $number=$_POST['text'];
+            
+
+            if(!is_numeric(($number))){
+                echo '
+                    <div class="mb-4 bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+                    <p class="font-bold">Be Warned</p>
+                    <p>Please enter a valid number not <u>'.$number.'</u></p>
+                    </div>
+                ';
+            }elseif($number<5){
+
+                echo'
+                    <div class="mb-4 bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+                    <div class="flex">
+                        <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+                        <div>
+                        <p class="font-bold">The number '.$number.' is less than 5</p>
+                        </div>
+                    </div>
+                    </div>
+                ';
+            }elseif($number>=5 && $number<9){
+                
+                echo'
+                    <div class="mb-4 bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+                    <div class="flex">
+                        <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+                        <div>
+                        <p class="font-bold">The number '.$number.' is between 5 and 9</p>
+                        </div>
+                    </div>
+                    </div>
+                ';
+            }elseif($number>=9){
+                
+                echo'
+                    <div class="mb-4 bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+                    <div class="flex">
+                        <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+                        <div>
+                        <p class="font-bold">The number '.$number.' is greater than 9</p>
+                        </div>
+                    </div>
+                    </div>
+                ';
+            }
+
+        }
+
+    ?>
+
+    
+
+    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <form class="space-y-6" method="post">
+            <h5 class="text-xl font-medium text-gray-900 dark:text-white">Analys numbers entered</h5>
+            <div>
+                <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter a number</label>
+                <input type="text" name="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="100" required />
+            </div>
+
+            <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
+
+        </form>
+    </div>
+
+    
+
+</body>
+</html>
+
--- Exercices/review/review-1.php
+++ Exercices/review/review-1.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <style>
+        body{
+            background-color: black;
+            color:white;
+        }
+    </style>
+</head>
+<body>
+    
+    <form method="post" >
+        <input type="text" name="text">
+        <button type="submit">Send</button>
+    </form>
+
+</body>
+</html>
+
 <?php 
 
     echo "hello world!"; 
@@ -5,8 +29,32 @@
     $name = "abdulrahman";
     $price = 10;
     echo "hello {$name}";
-    echo "<br>your pizza is \${$price}"
+    echo "<br>your pizza is \${$price}";
 
+    #functions 
+
+    if ( isset( $_POST['text'] ) && $_POST['text'] ) {
+        $number = $_POST['text'];
+        echo "<br>".round($number)."(round)<br>";
+        echo ceil($number)."(ceil)<br>";
+        echo floor($number)."(floor)<br>";
+        echo abs($number)."(abs '-' -> '+')<br>";
+
+        echo sqrt($number)."(sqrt)<br>";
+        echo pow($number,2)."(pow '**') ex **2<br>";
+        echo max($number,10,-10)." , max(this number,10,-10)<br>";
+        echo min($number,10,-10)." , max(this number,10,-10)<br>";
+        echo pi()." (PI==3.14)<br>";
+        echo rand(0,10)."rand(choose around between two number  ex 0-10 )";
+
+        
+
+        //round : if the number aftr "." >=5 one is added to the number befor "."  if <5 the part after the "." is being removed
+        //floor : (always down) delete the part after "." if the number is positive else one is added and delete last part
+        //ceil : (always up) added one and delete last part if the number is positive "+" else delete last part 
+
+    
+    }
     
 
 ?>
