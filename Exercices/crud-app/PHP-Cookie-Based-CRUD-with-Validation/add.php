97d707d345697967c169e424348a4a87ff7478dc
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-04-12 21:59:48 +0100

    PHP-cookie-Based-CRUD-with-VLD #5

--- Exercices/crud-app/PHP-Cookie-Based-CRUD-with-Validation/add.php
+++ Exercices/crud-app/PHP-Cookie-Based-CRUD-with-Validation/add.php
@@ -234,25 +234,8 @@
 
                 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                    <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
-                    <div class="mt-2">
-                        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                        <div class="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">workcation.com/</div>
-                        <input type="text" name="username" id="username" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="janesmith">
-                        </div>
-                    </div>
-                    </div>
 
-                    <div class="col-span-full">
-                    <label for="about" class="block text-sm/6 font-medium text-gray-900">About</label>
-                    <div class="mt-2">
-                        <textarea name="about" id="about" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
-                    </div>
-                    <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
-                    </div>
-
-                    <div class="col-span-full">
+                    <div class="col-span-2">
                     <label for="photo" class="block text-sm/6 font-medium text-gray-900">Photo</label>
                     <div class="mt-2 flex items-center gap-x-3">
                         <svg id="svgIcon" class="size-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon">
@@ -263,6 +246,28 @@
                         <button onclick="openFileDialog()" type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50">Change</button>
                     </div>
                     <input type="file" id="fileInput" name="image" style="display:none"  onchange="validateImage(this)">
+                    </div>
+
+
+                    <div class="sm:col-span-4">
+                    <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
+                    <div class="mt-2">
+                        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                        <div class="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">workcation.com/</div>
+                        <input type="text" name="username" id="username" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="janesmith">
+                        </div>
+                    </div>
+                    </div>
+
+        
+
+
+                    <div class="col-span-full">
+                    <label for="about" class="block text-sm/6 font-medium text-gray-900">About</label>
+                    <div class="mt-2">
+                        <textarea name="about" id="about" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                    </div>
+                    <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
                     </div>
 
                     <div class="col-span-full ">
@@ -346,15 +351,14 @@
                 const section = document.getElementById('uploadPDF');
                 if(!file) return;
 
-                if(!['application/pdf'].includes(file.type)){
-                    section.style.backgroundColor='red';
+                if(!['application/pdf'].includes(file.type) || file.size > 5*1024*1024){
+                    section.style.borderColor='red';
+                    return
+                }else{
+                    section.style.borderColor='gray';
                     return
                 }
 
-                if(file.size > 5*1024*1024){
-                    section.style.backgroundColor='red';
-                    return
-                }
 
 
         }
