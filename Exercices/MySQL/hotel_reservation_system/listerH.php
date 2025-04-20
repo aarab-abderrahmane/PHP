de793a9755fe4e316803ba47e3890c982fe5b960
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-04-20 21:51:03 +0100

    listerH.php (fix error)

--- Exercices/MySQL/hotel_reservation_system/listerH.php
+++ Exercices/MySQL/hotel_reservation_system/listerH.php
@@ -53,7 +53,7 @@
                 echo "<td>" . $row["id_hotel"] . "</td>";
                 echo "<td>" . $row["titre"] . "</td>";
                 echo "<td>" . $row["adresse"] . "</td>";
-                echo "<td>" . $row["prix_par_nuit"] . "</td>";
+                echo "<td>" . $row["prix_par_unit"] . "</td>";
                 echo "<td>" . $row["nombre_etoile"] . "</td>";
                 echo "<td>" . $row["nombre_de_places"] . "</td>";
                 echo "<td><a href='supprimerH.php?id=" . $row["id_hotel"] . "' onclick='return confirm(\"Êtes-vous sûr de vouloir supprimer cet hôtel?\")'>Supprimer</a></td>";
