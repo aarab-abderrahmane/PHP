73a2b7bcfd3eb1ee045b57d919f1a7eda958e8f1
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-04-22 13:14:08 +0100

    edir

--- Exercices/MySQL/hotel_reservation_system/ajouterH.php
+++ Exercices/MySQL/hotel_reservation_system/ajouterH.php
@@ -1,4 +1,12 @@
 <?php  
+        <!-- Categorie (idCat, libelle)
+        Todo(idTodo, text, #idCat,image,#idUser)
+        User(idUser, nom ,prenom,email,password,role (admin/user))
+        créer la base de données avec la contrainre FK
+        créer le fichier db.php
+        créer la page connexion.php 
+        un user va être redireger vers myTodos.php (qui afficher uniquement la liste de ses todos)  avec possibilité de delete and update
+        un admin vas être etre rediriger vers dashboardadmin.php  un select qui affiche la liste des users ( on change un table html va afficher tout les todos de l user selectionné ) et bien sur admin peut modifier et supprimer les todos -->
         
 
         include "database_connection.php";
