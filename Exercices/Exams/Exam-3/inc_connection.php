25ca398cc588cef418ab75eb4e7d3f8382d5575a
abdulrahman <abdulrahman.tie@gmail.com>
2025-05-10 23:44:41 +0100

    exercice-2 (exam 3)

---
+++ Exercices/Exams/Exam-3/inc_connection.php
@@ -0,0 +1,31 @@
+<?php
+
+    include 'inc_config.php';
+    $db_name = db_name;
+
+    try{
+
+        $conn = new PDO("mysql:host=".db_host,db_user,db_pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+        $conn->exec('CREATE DATABASE IF NOT EXISTS '.$db_name);
+        $conn->exec("USE " . db_name);
+
+        $query = "CREATE TABLE IF NOT EXISTS Fournisseurs(
+                    idFourn INT PRIMARY KEY ,
+                    nomFourn VARCHAR(50) NOT NULL ,
+                    prenomFourn VARCHAR(100) NOT NULL,
+                    adrFourn TEXT NOT NULL ,
+                    emailForun VARCHAR(200) NOT NULL,
+                    telForun INT NOT NULL 
+                );
+        ";
+
+        $conn->exec($query);
+
+
+    }catch(PDOException $e){
+        echo "Error : ".$e->getMessage();
+    }
+
+?>
